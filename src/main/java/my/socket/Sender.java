7158753c3300dc0fc9705e4f329ae155340e2003
Квасов Сергей Сергеевич <sskvasov@dev.vtb.ru>
2021-08-12 10:50:07 +0300

    first

--- src/main/java/my/socket/Sender.java
+++ src/main/java/my/socket/Sender.java
@@ -1,7 +1,19 @@
 package my.socket;
 
-public class Sender {
-    public static void main(String[] args) {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
+public class Sender {
+    public static void main(String[] args) throws IOException {
+        int port = 1122;
+        String ip = "127.0.0.1";
+        Socket socket = new Socket(ip, port);
+        String data = "Hello, Socket!";
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        writer.write(data + "\n");
+        writer.close();
+        socket.close();
     }
 }
